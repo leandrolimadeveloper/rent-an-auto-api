32ebe42f7829a35db7bc1713a8b5c1c72b4b6454
leandrolimadeveloper <leandroslimasr@gmail.com>
2023-02-16 13:29:19 -0300

    Created doc

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './swagger.json'
 import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(routes)
 
