990a2276ea4c3218b3b8e53bbf16c181c38ef113
leandrolimadeveloper <leandroslimasr@gmail.com>
2023-04-13 15:13:16 -0300

    Created migration for car specification

---
+++ src/shared/infra/typeorm/migrations/1681399530326-AlterIdTypeToUuid.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class AlterIdTypeToUuid1681399530326 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.changeColumn(
+            'cars',
+            'id',
+            new TableColumn({
+                name: 'id',
+                type: 'uuid',
+                isPrimary: true,
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.changeColumn(
+            'cars',
+            'id',
+            new TableColumn({
+                name: 'id',
+                type: 'varchar',
+            })
+        );
+    }
+}
