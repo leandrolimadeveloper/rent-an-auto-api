83f9f5add8c77220be749e8a332555f5216c226e
leandrolimadeveloper <leandroslimasr@gmail.com>
2023-04-19 16:56:21 -0300

    Created UploadCarImages use case

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -8,6 +8,8 @@ import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/Us
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { CarsImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository';
+import { ICarsImagesRepository } from '@modules/cars/repositories/ICarsImagesRepository';
 
 container.registerSingleton<ICategoriesRepository>('CategoriesRepository', CategoriesRepository);
 
@@ -16,3 +18,5 @@ container.registerSingleton<ISpecificationsRepository>('SpecificationsRepository
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
 
 container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
+
+container.registerSingleton<ICarsImagesRepository>('CarsImagesRepository', CarsImagesRepository);
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,20 +1,34 @@
 import { Router } from 'express';
-
-import { CreateCarController } from '@modules/cars/useCases/Car/createCar/CreateCarController';
-import { ListAvailableCarsController } from '@modules/cars/useCases/Car/listAvailableCars/ListAvailableCarsController';
-import { CreateCarSpecificationController } from '@modules/cars/useCases/Specification/CreateCarSpecification/CreateCarSpecificationController';
+import multer from 'multer';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 
+import uploadConfig from '@config/upload';
+
+import { CreateCarController } from '@modules/cars/useCases/Car/createCar/CreateCarController';
+import { ListAvailableCarsController } from '@modules/cars/useCases/Car/listAvailableCars/ListAvailableCarsController';
+import { CreateCarSpecificationController } from '@modules/cars/useCases/Specification/CreateCarSpecification/CreateCarSpecificationController';
+import { UploadCarImagesController } from '@modules/cars/useCases/Car/uploadCarImages/UploadCarImagesController';
+
 const carsRoutes = Router();
+
+const uploadCarImages = multer(uploadConfig.upload('./tmp/cars'));
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationsController = new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImagesController();
 
 carsRoutes.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
 carsRoutes.get('/available', listAvailableCarsController.handle);
 carsRoutes.post('/specifications/:id', ensureAuthenticated, ensureAdmin, createCarSpecificationsController.handle);
+carsRoutes.post(
+    '/images/:id',
+    ensureAuthenticated,
+    ensureAdmin,
+    uploadCarImages.array('images'),
+    uploadCarImagesController.handle
+);
 
 export { carsRoutes };
